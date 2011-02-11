f651c9c8b83eb7d06dd8a52841c15e1591a02d42
s1nz <a.poplavskij@gmail.com>
2011-02-11 18:24:08 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tviymoto::Application.config.secret_token = '173f7a0b6f14ccb38404a63fcc0d968a04ff898310e1375edd78268734457fd750120c715fc41879e9df5f517445179db5a706943070910929646475b14a11d8'
